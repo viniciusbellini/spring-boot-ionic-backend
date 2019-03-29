af24947665711dbe5842fe477f101192494c45ba
Vinicius Bellini <viniciusbellini@gmail.com>
2019-03-29 13:37:19 -0300

    Criando Repository e Service para Categoria

---
+++ src/main/java/com/bellini/cursomc/services/CategoriaService.java
@@ -0,0 +1,20 @@
+package com.bellini.cursomc.services;
+
+import com.bellini.cursomc.domain.Categoria;
+import com.bellini.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository repo;
+
+    public Categoria buscar(Integer id){
+        Optional<Categoria> obj = repo.findById(id);
+        return obj.orElse(null);
+    }
+}

3ebf6f47df66ff6722a436e18ac65273e2587cf8
Vinicius Bellini <viniciusbellini@gmail.com>
2019-03-29 15:24:55 -0300

    Ajustes no endpoint /categoria/{id}

--- src/main/java/com/bellini/cursomc/services/CategoriaService.java
+++ src/main/java/com/bellini/cursomc/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.bellini.cursomc.services;
 
 import com.bellini.cursomc.domain.Categoria;
 import com.bellini.cursomc.repositories.CategoriaRepository;
+import com.bellini.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,8 @@ public class CategoriaService {
 
     public Categoria buscar(Integer id){
         Optional<Categoria> obj = repo.findById(id);
-        return obj.orElse(null);
+
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+                + ", Tipo: " + Categoria.class.getName()));
     }
 }

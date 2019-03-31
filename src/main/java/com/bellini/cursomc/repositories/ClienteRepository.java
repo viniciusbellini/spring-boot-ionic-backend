9d74047fe5b57cd0c40785bae2e756bb9cfc2822
Vinicius Bellini <viniciusbellini@gmail.com>
2019-03-31 15:15:07 -0300

    Cliente, TipoCliente, Telefones e Endereco

---
+++ src/main/java/com/bellini/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.bellini.cursomc.repositories;
+
+import com.bellini.cursomc.domain.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+
+
+}
---
+++ src/main/java/com/bellini/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.bellini.cursomc.repositories;
+
+import com.bellini.cursomc.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+
+}

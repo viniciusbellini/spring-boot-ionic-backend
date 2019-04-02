53880d86e7c8b8d958551d60c2bb0a055cdb1049
Vinicius Bellini <viniciusbellini@gmail.com>
2019-04-01 22:46:46 -0300

    Inserindo um novo cliente com POST

--- src/main/java/com/bellini/cursomc/resources/ClienteResource.java
+++ src/main/java/com/bellini/cursomc/resources/ClienteResource.java
@@ -1,20 +1,18 @@
 package com.bellini.cursomc.resources;
 
-import com.bellini.cursomc.domain.Cliente;
+import com.bellini.cursomc.domain.Categoria;
 import com.bellini.cursomc.domain.Cliente;
 import com.bellini.cursomc.dto.ClienteDTO;
+import com.bellini.cursomc.dto.ClienteNewDTO;
 import com.bellini.cursomc.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -30,6 +28,15 @@ public class ClienteResource {
 
         Cliente obj = service.find(id);
         return ResponseEntity.ok(obj);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDTO){
+        Cliente obj = service.fromDTO(objDTO);
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
--- src/main/java/com/bellini/cursomc/services/ClienteService.java
+++ src/main/java/com/bellini/cursomc/services/ClienteService.java
@@ -1,9 +1,14 @@
 package com.bellini.cursomc.services;
 
+import com.bellini.cursomc.domain.Cidade;
 import com.bellini.cursomc.domain.Cliente;
-import com.bellini.cursomc.domain.Cliente;
+import com.bellini.cursomc.domain.Endereco;
+import com.bellini.cursomc.domain.enums.TipoCliente;
 import com.bellini.cursomc.dto.ClienteDTO;
+import com.bellini.cursomc.dto.ClienteNewDTO;
+import com.bellini.cursomc.repositories.CidadeRepository;
 import com.bellini.cursomc.repositories.ClienteRepository;
+import com.bellini.cursomc.repositories.EnderecoRepository;
 import com.bellini.cursomc.services.exceptions.DataIntegrityException;
 import com.bellini.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +17,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -20,19 +26,30 @@ import java.util.Optional;
 public class ClienteService {
 
     @Autowired
-    private ClienteRepository repo;
+    private ClienteRepository clienteRepository;
+
+    @Autowired
+    private EnderecoRepository enderecoRepository;
 
     public Cliente find(Integer id){
-        Optional<Cliente> obj = repo.findById(id);
+        Optional<Cliente> obj = clienteRepository.findById(id);
 
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id
                 + ", Tipo: " + Cliente.class.getName()));
     }
-    
+
+    @Transactional
+    public Cliente insert(Cliente obj) {
+        obj.setId(null);
+        obj = clienteRepository.save(obj);
+        enderecoRepository.saveAll(obj.getEnderecos());
+        return obj;
+    }
+
     public Cliente update(Cliente obj) {
         Cliente newObj = find(obj.getId());
         updateData(newObj,obj);
-        return repo.save(newObj);
+        return clienteRepository.save(newObj);
     }
 
     private void updateData(Cliente newObj, Cliente obj) {
@@ -43,7 +60,7 @@ public class ClienteService {
     public void delete(Integer id) {
         find(id);
         try{
-            repo.deleteById(id);
+            clienteRepository.deleteById(id);
         }
         catch (DataIntegrityViolationException e){
             throw new DataIntegrityException("Não é possivel excluir porque há entidades relacionadas");
@@ -52,15 +69,33 @@ public class ClienteService {
     }
 
     public List<Cliente> findAll() {
-        return repo.findAll();
+        return clienteRepository.findAll();
     }
 
     public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
         PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
-        return repo.findAll(pageRequest);
+        return clienteRepository.findAll(pageRequest);
     }
 
     public Cliente fromDTO(ClienteDTO objDTO){
         return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
     }
+
+    public Cliente fromDTO(ClienteNewDTO objDTO){
+        Cliente cli = new Cliente(null, objDTO.getNome(), objDTO.getEmail(), objDTO.getCpfOuCnpj(), TipoCliente.toEnum(objDTO.getTipo()));
+        Cidade cid = new Cidade(objDTO.getCidadeId(), null, null);
+        Endereco end = new Endereco(null, objDTO.getLogradouro(), objDTO.getNumero(),
+                objDTO.getComplemento(), objDTO.getBairro(), objDTO.getCep(), cli, cid);
+        cli.getEnderecos().add(end);
+        cli.getTelefones().add(objDTO.getTelefone1());
+
+        if (objDTO.getTelefone2() != null)
+            cli.getTelefones().add(objDTO.getTelefone2());
+
+        if (objDTO.getTelefone3() != null)
+            cli.getTelefones().add(objDTO.getTelefone3());
+
+        return cli;
+    }
+
 }

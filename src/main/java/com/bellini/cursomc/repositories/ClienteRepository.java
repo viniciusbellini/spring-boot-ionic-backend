5a29fe420a21bb2969b1961fb506f5255eab14b6
Vinicius Bellini <viniciusbellini@gmail.com>
2019-04-03 08:54:37 -0300

    Validação customizada: email não repetido na insercão de Cliente

--- src/main/java/com/bellini/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/bellini/cursomc/repositories/ClienteRepository.java
@@ -1,11 +1,14 @@
 package com.bellini.cursomc.repositories;
 
 import com.bellini.cursomc.domain.Cliente;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
-
+    @Transactional(readOnly = true)
+    Cliente findByEmail(String email);
 }
--- src/main/java/com/bellini/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/bellini/cursomc/services/validation/ClienteInsertValidator.java
@@ -1,9 +1,12 @@
 package com.bellini.cursomc.services.validation;
 
+import com.bellini.cursomc.domain.Cliente;
 import com.bellini.cursomc.domain.enums.TipoCliente;
 import com.bellini.cursomc.dto.ClienteNewDTO;
+import com.bellini.cursomc.repositories.ClienteRepository;
 import com.bellini.cursomc.resources.exceptions.FieldMessage;
 import com.bellini.cursomc.services.validation.utils.BR;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
@@ -11,6 +14,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ClienteInsertValidator implements ConstraintValidator <ClienteInsert, ClienteNewDTO> {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
 
     @Override
     public void initialize(ClienteInsert constraintAnnotation) {
@@ -26,6 +32,11 @@ public class ClienteInsertValidator implements ConstraintValidator <ClienteInser
 
         if (objDTO.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDTO.getCpfOuCnpj())){
             list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+        }
+
+        Cliente aux = clienteRepository.findByEmail(objDTO.getEmail());
+        if (aux != null){
+            list.add(new FieldMessage("email", "Email já existente"));
         }
 
         for (FieldMessage e : list) {

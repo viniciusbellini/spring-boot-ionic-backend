6ada71b2ae09b22b560a6812cbda6da1d974eff5
Vinicius Bellini <viniciusbellini@gmail.com>
2019-04-02 22:40:29 -0300

    Ajustando e testando a delecao de Cliente

--- src/main/java/com/bellini/cursomc/domain/Cliente.java
+++ src/main/java/com/bellini/cursomc/domain/Cliente.java
@@ -3,13 +3,7 @@ package com.bellini.cursomc.domain;
 import com.bellini.cursomc.domain.enums.TipoCliente;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import javax.persistence.CollectionTable;
-import javax.persistence.ElementCollection;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -34,7 +28,7 @@ public class Cliente implements Serializable {
     @OneToMany(mappedBy = "cliente")
     private List<Pedido> pedidos = new ArrayList<>();
 
-    @OneToMany(mappedBy = "cliente")
+    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
     private List<Endereco> enderecos = new ArrayList<>();
 
     @ElementCollection
